4c0088d537949b6f1e2b9bcaadf7b9c4dcd4043c
andrey <larginator@gmail.com>
2023-07-14 16:30:48 +0600

    9. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -15,4 +15,32 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item",
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test item");
+        tracker.add(item);
+        assertThat(item.getId())
+                .isEqualTo(tracker
+                        .findById(item.getId())
+                        .getId()
+                );
+        tracker.delete(item.getId());
+        assertThat(tracker.findById(item.getId())).isEqualTo(null);
+    }
 }
