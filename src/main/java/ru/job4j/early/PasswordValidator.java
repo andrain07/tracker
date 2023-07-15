3855c00f06a145a0e9279884ff664a28e3ad1b9d
andrey <larginator@gmail.com>
2023-07-15 21:47:20 +0600

    4. Принципы раннего возврата и охранных выражений [#504875]

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,80 @@
+package ru.job4j.early;
+
+public class PasswordValidator {
+    private static final String[] FORBIDDEN = {"qwerty", "12345", "password", "admin", "user"};
+
+    /**
+     * Метод проверят пароль по правилам:
+     * 1. Если password null, то выбросить исключение "Password can't be null";
+     * 2. Длина пароля находится в диапазоне [8, 32],
+     * если нет то "Password should be length [8, 32]";
+     * 3. Пароль содержит хотя бы один символ в верхнем регистре,
+     * если нет то "Password should contain at least one uppercase letter");
+     * 4. Пароль содержит хотя бы один символ в нижнем регистре,
+     * если нет то "Password should contain at least one lowercase letter";
+     * 5. Пароль содержит хотя бы одну цифру,
+     * если нет то"Password should contain at least one figure");
+     * 6. Пароль содержит хотя бы один спец. символ (не цифра и не буква),
+     * если нет то "Password should contain at least one special symbol");
+     * 7. Пароль не содержит подстрок без учета регистра: qwerty, 12345, password, admin, user.
+     * Без учета регистра, значит что, например, ни qwerty ни QWERTY ни qwErty и т.п.
+     * если да, то "Password shouldn't contain substrings: qwerty, 12345, password, admin, user".
+     *
+     * @param password Пароль
+     * @return Вернет пароль или выбросит исключение.
+     */
+    public static String validate(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password can't be null");
+        }
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException("Password should be length [8, 32]");
+        }
+        String[] banStrings = new String[]{"qwerty", "12345", "password", "admin", "user"};
+        for (String banString : banStrings) {
+            if (password.toLowerCase().contains(banString)) {
+                throw new IllegalArgumentException(
+                        "Password shouldn't contain substrings:"
+                                + " qwerty, 12345, password, admin, user");
+            }
+        }
+        boolean hasUpCase = false;
+        boolean hasLowCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+        String specials = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+        for (char symbol : password.toCharArray()) {
+            if (!hasUpCase && Character.isUpperCase(symbol)) {
+                hasUpCase = true;
+            } else if (!hasLowCase && Character.isLowerCase(symbol)) {
+                hasLowCase = true;
+            } else if (!hasDigit && Character.isDigit(symbol)) {
+                hasDigit = true;
+            } else if (!hasSpecial
+                    && specials.contains(String.valueOf(symbol))) {
+                hasSpecial = true;
+            }
+        }
+        if (!hasUpCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one uppercase letter"
+            );
+        }
+        if (!hasLowCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one lowercase letter"
+            );
+        }
+        if (!hasDigit) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one figure"
+            );
+        }
+        if (!hasSpecial) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one special symbol"
+            );
+        }
+        return password;
+    }
+}
