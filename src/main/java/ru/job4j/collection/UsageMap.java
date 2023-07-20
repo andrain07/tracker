90b175d01098912b534549eb4bdc2559e4e4cb74
andrey <larginator@gmail.com>
2023-07-20 20:11:53 +0600

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,6 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> accounts = new HashMap<>();
         accounts.put("vasyapupkin@gmail.com", "Vasya Pupkin");
-        accounts.put("vasyapudpkin@gmail.com", "Vasya Pupkin");
         for (String personEmail : accounts.keySet()) {
             System.out.printf("Email: %s | Full name: %s\n",
                     personEmail,
