df9ee36ef08dc72eea9e9d4854cdd1daae0557b0
andrey <larginator@gmail.com>
2023-07-28 18:30:27 +0600

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -6,16 +6,36 @@ import java.util.List;
 import java.util.Map;
 
 public class BankService {
+    /**
+     * Поле содержит всех пользователей системы с привязанными к ним счетами
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Добавляется нового пользователя в сисетему
+     *
+     * @param user новый пользовать
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Удаляет существующего в системе пользователя по паспортным данным
+     *
+     * @param passport паспортные данные удаляемого пользователя
+     * @return true, если пользователь успешно удален, иначе - false
+     */
     public boolean deleteUser(String passport) {
         return users.remove(new User(passport, "")) != null;
     }
 
+    /**
+     * Добавляет новый счет к существующему пользователю
+     *
+     * @param passport паспортные данные пользователя
+     * @param account  добавляемый счет
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         var userAccounts = users.get(user);
@@ -24,6 +44,12 @@ public class BankService {
         }
     }
 
+    /**
+     * Ищет в системе существующего пользователя по паспортным данным
+     *
+     * @param passport паспортные данные искомого пользователя
+     * @return найденного User или null, если пользователь не найден
+     */
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (user.getPassport().equals(passport)) {
@@ -33,6 +59,12 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Ищет счет существующего пользователя по реквезитам счета
+     *
+     * @param passport паспортные данные удаляемого пользователя
+     * @return найденный Account или null, если счет не найден
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -46,6 +78,15 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Перечисляет деньги со счета одного пользователю другому пользователю
+     *
+     * @param srcPassport паспортные данные счета отправителя
+     * @param srcRequisite реквизиты счета отправителя
+     * @param destPassport паспортные данные счета получателя
+     * @param destRequisite реквизиты счета получателя
+     * @return true в случае успешного перевода, иначе - false
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         Account srcAccount = findByRequisite(srcPassport, srcRequisite);
@@ -60,6 +101,12 @@ public class BankService {
         return true;
     }
 
+    /**
+     * Возвращает список всех счетов пользователя
+     *
+     * @param user объект пользователя
+     * @return список всех счетов пользователя
+     */
     public List<Account> getAccounts(User user) {
         return users.get(user);
     }
