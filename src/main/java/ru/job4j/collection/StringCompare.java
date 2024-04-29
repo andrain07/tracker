bce362254c9687ee45d10cb421f237939e5d0505
larginator <larginator@gmail.com>
2024-04-29 21:08:01 +0600

    3. Компаратор для строк [#35008 #432843]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,13 +5,9 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        char leftChar;
-        char rightChar;
         for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
-            leftChar = left.charAt(i);
-            rightChar = right.charAt(i);
-            if (leftChar != rightChar) {
-                return Character.compare(leftChar, rightChar);
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(left.charAt(i), right.charAt(i));
             }
         }
         return Integer.compare(left.length(), right.length());
