f0e144fa5b978b5594dae894b9a8cae6ff489acd
larginator <larginator@gmail.com>
2024-04-30 13:55:45 +0600

    3. Отсортировать департаменты [#34352 #433034]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,18 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] split1 = str1.split("/", 2);
         String[] split2 = str2.split("/", 2);
         int firstComparison = split2[0].compareTo(split1[0]);
-        if (firstComparison != 0) {
-            return firstComparison;
-        } else {
-            if (split1.length > 1 && split2.length > 1) {
-                return split1[1].compareTo(split2[1]);
-            } else if (split1.length > 1) {
-                return 1;
-            } else if (split2.length > 1) {
-                return -1;
-            } else {
-                return 0;
-            }
-        }
+        return firstComparison != 0 ? firstComparison : str1.compareTo(str2);
     }
 }
